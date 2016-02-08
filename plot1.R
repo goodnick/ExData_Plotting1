b06d5aebab4f0d93359bd4470bec7e60daca1c0a
Nick Menere <goodnick@gmail.com>
2016-02-08 13:36:33 +1100

    Refactor - seperating plot generation and file generation

--- plot1.R
+++ plot1.R
@@ -2,14 +2,18 @@ if(!exists("readHousePowerConsumptionData", mode="function")) {
     source("ReadData.R")
 }
 
-createPlot1 <- function () {
+createPlot1File <- function () {
     filename <- "plot1.png"
     png(filename = filename, bg = "transparent")
+    generatePlot1()
+    dev.off()
+    message(paste("plot1 created -", filename))
 
+}
+
+generatePlot1 <- function() {
     houseData <- readHousePowerConsumptionData()
     hist(houseData$Global_active_power, col = "red", main = "Global Active Power",
          xlab = "Global Active Power (kilowatts)")
 
-    dev.off()
-    message(paste("plot1 created -", filename))
 }
--- plot2.R
+++ plot2.R
@@ -2,14 +2,19 @@ if(!exists("readHousePowerConsumptionData", mode="function")) {
     source("ReadData.R")
 }
 
-createPlot2 <- function () {
+createPlot2File <- function() {
     filename <- "plot2.png"
     png(filename = filename, bg = "transparent")
+    generatePlot2()
 
+    dev.off()
+    message(paste("plot2 created -", filename))
+
+}
+
+generatePlot2 <- function () {
     houseData <- readHousePowerConsumptionData()
     plot(houseData$Time, houseData$Global_active_power, type="l",
          xlab = "", ylab = "Global Active Power (kilowatts)")
 
-    dev.off()
-    message(paste("plot2 created -", filename))
 }
